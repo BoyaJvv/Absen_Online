e5e423eb2c097a81270286defc2592394fb849fd
boyajvv <boyarizky786@gmail.com>
2026-01-14 12:04:26 +0700

    add mesin bldae dan tampilan layouts

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -39,7 +39,7 @@ class AuthenticatedSessionController extends Controller
             
             $request->session()->regenerate();
 
-            return redirect()->intended(route('dashboard', absolute: false));
+            return redirect()->intended(route('index', absolute: false));
         }
 
         // 4. Jika gagal (user tidak ditemukan atau password salah)
